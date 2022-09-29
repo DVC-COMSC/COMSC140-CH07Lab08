19507052193a49e98f330527cf165bfd9b6a1058
Kyuwoong Lee <kwooler@gmail.com>
2022-09-28 21:52:07 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-
-inputvalues = input('Enter 5 numbers: ')
-numbers = inputvalues.split() 
-for i in range(len(numbers)):
-	numbers[i] = int(numbers[i]) 
-# The following line is the same as the for-loop
-# numbers = list(map(int, numbers))
-
 # ******************************
 # Make your Code
 # ******************************
